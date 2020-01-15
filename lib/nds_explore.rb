c44fbae078b3dea52778f3a3b9093aa22fccfb59
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-15 18:41:40 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,8 +12,8 @@ def print_first_directors_movie_titles
   movies = directors_database[0][:movies]
   index = 0
     
-while index < ss_movies.length do
-    titles = ss_movies[index][:title]
+while index < movies.length do
+    titles = movies[index][:title]
     puts titles
     index +=1
   end
